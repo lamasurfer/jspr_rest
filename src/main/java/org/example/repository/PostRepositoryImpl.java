69305b0b70a600096b8a7fc85abc403bb7c745bb
lamasurfer <7376771@gmail.com>
2021-02-06 14:29:26 +0300

    initial commit

---
+++ src/main/java/org/example/repository/PostRepositoryImpl.java
@@ -0,0 +1,34 @@
+package org.example.repository;
+
+
+import org.example.model.Post;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Repository
+public class PostRepositoryImpl implements PostRepository {
+
+    private final Map<Long, Post> posts = new ConcurrentHashMap<>();
+
+    public List<Post> all() {
+        return new ArrayList<>(posts.values());
+    }
+
+    public Optional<Post> getById(long id) {
+        return Optional.ofNullable(posts.get(id));
+    }
+
+    public Post save(Post post) {
+        posts.put(post.getId(), post);
+        return post;
+    }
+
+    public void removeById(long id) {
+        posts.remove(id);
+    }
+}
---
+++ src/main/java/org/example/service/PostService.java
@@ -0,0 +1,46 @@
+package org.example.service;
+
+
+import org.example.exception.NotFoundException;
+import org.example.model.Post;
+import org.example.repository.PostRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Service
+public class PostService {
+    private final AtomicLong counter = new AtomicLong(1);
+    private final PostRepository repository;
+
+    public PostService(PostRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Post> all() {
+        return repository.all();
+    }
+
+    public Post getById(long id) {
+        return repository.getById(id).orElseThrow(NotFoundException::new);
+    }
+
+    public Post save(Post post) {
+        final var id = post.getId();
+        final var optionalPost = repository.getById(id);
+        if (id != 0 && optionalPost.isPresent()) {
+            final var oldPost = optionalPost.get();
+            oldPost.setContent(post.getContent());
+            return repository.save(oldPost);
+        } else {
+            post.setId(counter.getAndIncrement());
+            return repository.save(post);
+        }
+    }
+
+    public void removeById(long id) {
+        repository.removeById(id);
+    }
+}
+
